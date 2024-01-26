ab0e03a5c24b1c07867351c01195abbfdb394d69
Federico Mastroforti <129280581+Feddex@users.noreply.github.com>
2024-01-26 18:42:30 +0100

    editing comments

--- Configuration.hpp
+++ Configuration.hpp
@@ -1,19 +1,6 @@
 #ifndef CONFIGURATION_HPP
 #define CONFIGURATION_HPP
 
-// #include <cassert>
-// #include <cmath>
-// #include <iostream>
-// #include <vector>
 
-// #include "BCalc.hpp"
-// #include "DataGen.hpp"
-// #include "Vec.hpp"
-
-// class Configuration {
-//  public:
-//   // step will be the coil density (we will have one coil per step)
-//   static std::vector<Coil> config1(Coil const &coil);
-// };
 
 #endif
